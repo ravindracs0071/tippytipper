379ff931b2292ffb97a04d04c1e7ffef60fae28a
mandlar <mandlar@5fd87178-0ee9-11df-8a19-6d7f5c3cf6d3>
2010-03-07 14:46:59 +0000

    Added more unit test cases.

--- trunk/Tippy Tipper/src/net/mandaria/services/TipCalculatorService.java
+++ trunk/Tippy Tipper/src/net/mandaria/services/TipCalculatorService.java
@@ -1,5 +1,7 @@
 package net.mandaria.services;
 
+import java.text.NumberFormat;
+
 import net.mandaria.R;
 import net.mandaria.TippyTipperApplication;
 import android.widget.EditText;
@@ -8,9 +10,9 @@ import android.widget.TextView;
 public class TipCalculatorService 
 {
 	private String BillEntry = "";
-	private String BillAmount = "";
-	private String TipAmount = "";
-	private String TotalAmount = "";
+	private double BillAmount = 0;
+	private double TipAmount = 0;
+	private double TotalAmount = 0;
 	
 	public TipCalculatorService()
 	{
@@ -19,7 +21,9 @@ public class TipCalculatorService
 	
 	public String GetBillAmount() 
 	{
-        return "$" + BillAmount;
+		NumberFormat nf = NumberFormat.getCurrencyInstance();
+		return nf.format(BillAmount);
+        //return "$" + BillAmount;
     }
 	
 	public String GetTipAmount()
@@ -39,10 +43,13 @@ public class TipCalculatorService
     
     public void AppendNumberToBillAmount(String number)
     {
-    	BillEntry = BillEntry + "" + number;
+    	if(BillEntry.length() < 16)
+    	{
+	    	BillEntry = BillEntry + "" + number;
+    	}
     	double amount = Double.valueOf(BillEntry);
     	amount = amount / 100;
-    	BillAmount = Double.toString(amount);    	
+    	BillAmount = amount; 
     }
     
     public void RemoveEndNumberFromBillAmount()
@@ -52,19 +59,19 @@ public class TipCalculatorService
     		BillEntry = BillEntry.substring(0, BillEntry.length() - 1);
     		double amount = Double.valueOf(BillEntry);
     		amount = amount / 100;
-    		BillAmount = Double.toString(amount);
+    		BillAmount = amount;
     	}
     	else 
     	{
     		BillEntry = "";
-    		BillAmount = "";
+    		BillAmount = 0;
 		}
     }
     
     public void ClearBillAmount()
     {
     	BillEntry = "";
-    	BillAmount = "";
+    	BillAmount = 0;
     }
     
     public void CalculateTip(double percent)
@@ -75,8 +82,8 @@ public class TipCalculatorService
   		double tip_amount = bill_amount * percent;
   		double total_amount = bill_amount + tip_amount; 
   		
-  		TipAmount = Double.toString(tip_amount);
-  		TotalAmount = Double.toString(total_amount);
+  		TipAmount = tip_amount;
+  		TotalAmount = total_amount;
   		
     }
     
