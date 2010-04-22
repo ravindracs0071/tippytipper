4d06149aa2755648ad71f95658d6680b9da3f600
mandlar <mandlar@5fd87178-0ee9-11df-8a19-6d7f5c3cf6d3>
2010-04-22 00:36:03 +0000

    Hooked slider into updating tip percent, tip amount, and total amount.

--- trunk/Tippy Tipper/src/net/mandaria/Total.java
+++ trunk/Tippy Tipper/src/net/mandaria/Total.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.*;
 import android.view.View.OnClickListener;
 import android.widget.*;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class Total extends Activity {
 
@@ -14,9 +15,44 @@ public class Total extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.total);  
-        
-        TippyTipperApplication appState = ((TippyTipperApplication)this.getApplication());
-        appState.service.CalculateTip(0.15);
+		
+        CalculateTip(15.0);
+		
+		
+		SeekBar seek_tip_percentage = (SeekBar)findViewById(R.id.seek_tip_percentage);
+		seek_tip_percentage.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
+			{	
+				@Override
+				public void onProgressChanged(SeekBar seekBar, int progress,
+						boolean fromUser)
+				{
+					CalculateTip((double)progress);
+				}
+
+				@Override
+				public void onStartTrackingTouch(SeekBar seekBar)
+				{
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void onStopTrackingTouch(SeekBar seekBar)
+				{
+					// TODO Auto-generated method stub
+					
+				}
+			});
+		
+    }
+    
+    private void CalculateTip(Double percent)
+    {
+    	TextView lbl_tip_percentage = (TextView)findViewById(R.id.lbl_tip_percentage);
+		lbl_tip_percentage.setText(percent + "%");
+		
+		TippyTipperApplication appState = ((TippyTipperApplication)this.getApplication());
+		appState.service.CalculateTip(percent/100);
 		
 		TextView lbl_amount = (TextView)findViewById(R.id.lbl_amount);
 		TextView lbl_tip = (TextView)findViewById(R.id.lbl_tip);
@@ -25,6 +61,5 @@ public class Total extends Activity {
 		lbl_amount.setText(appState.service.GetBillAmount());
 		lbl_tip.setText(appState.service.GetTipAmount());
 		lbl_total.setText(appState.service.GetTotalAmount());
-		
     }
 }
